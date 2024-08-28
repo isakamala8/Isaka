e63cef67e0e7638dfadbaf733a7824623f4e062f
isakamala8 <165497706+isakamala8@users.noreply.github.com>
2024-08-28 20:32:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,22 +12,22 @@ global.autoread = true // true a i dah chuan message zawng zawng read vek zelin 
 global.autoread_status = true // true a i dah chuan auto status view in a awm ang
 
 //info
-global.owner = ['918416093656'] // I phone number in thlak rawh
-global.ownernumber = '918416093656' // I phone number in thlak rawh
-global.ownername = "꧁Herbert♕꧂" // I hming dah tel bawk la
-global.location = "India, Mizoram, Aizawl" // I awmna hmun daha
+global.owner = ['6033308905'] // I phone number in thlak rawh
+global.ownernumber = '6033308905' // I phone number in thlak rawh
+global.ownername = "꧁Isaka♕꧂" // I hming dah tel bawk la
+global.location = "Durtlang, Mizoram, Aizawl" // I awmna hmun daha
 global.botname = "HBWABot Mizo" // I bot hming tur dah la
 global.ownerweb = "https://youtube.com/@HBMods_Channel"
 global.themeemoji = '🤖'
-global.creator = "918416093656@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
+global.creator = "6033308905@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
 
-global.ownerNumber = ["918416093656@s.whatsapp.net"]// @ symbol hmaa number hi i phone number in thlak rawh
+global.ownerNumber = ["6033308905@s.whatsapp.net"]// @ symbol hmaa number hi i phone number in thlak rawh
 global.prefixc = ['/'] // Prefix a tana hman theih loh tur symbolise a awm nuala, i bot a work a lo a nih chuan i thlak mai dawn nia
 global.sessionName = 'session'
 
 // Sticker Pack Name
 global.packname = "Sticker By"
-global.author = "HBWABot"
+global.author = "isak_m.s.d.a"
 
 //BUTTON INPUT 
 global.buttonyt = "SUBSCRIBE"
